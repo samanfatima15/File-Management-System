7fdcab5a1261effa5c41884a1613ca77816be067
Ayesha77314 <ayesharafique1049@gmail.com>
2026-04-24 14:13:15 +0500

    Update node.cpp

--- node.cpp
+++ node.cpp
@@ -1,29 +1,24 @@
 #include "Node.h"
 
-// Constructor
 Node::Node(const string& name, Node* parent)
     : name(name), parent(parent) {
 }
 
-// Destructor
 Node::~Node() {}
 
-// Get name
+
 string Node::getName() const {
     return name;
 }
 
-// Get parent
+
 Node* Node::getParent() const {
     return parent;
 }
-
-// Set name
 void Node::setName(const string& newName) {
     name = newName;
 }
 
-// Build full path recursively
 string Node::getPath() const {
     // Root case
     if (parent == nullptr)
