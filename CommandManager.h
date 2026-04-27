fd7a5a6bff84f90091840f0789b074543b4fdc7a
samanfatima15 <samanfatima768@gmail.com>
2026-04-27 23:46:39 +0500

    Add files via upload

---
+++ CommandManager.h
@@ -0,0 +1,30 @@
+//#pragma once
+//#ifndef COMMANDMANAGER_H
+//#define COMMANDMANAGER_H
+//
+//#include "Folder.h"
+//#include <string>
+//using namespace std;
+//
+//class CommandManager {
+//private:
+//    Folder* root;
+//    Folder* current;
+//
+//    void searchRecursive(Folder* folder, const string& targetName, const string& path);
+//
+//public:
+//    CommandManager();
+//    ~CommandManager();
+//
+//    void ls();
+//    void mkdir(const string& name);
+//    void cd(const string& name);
+//    void rm(const string& name);
+//    void rename(const string& oldName, const string& newName);
+//    void search(const string& name);
+//    void touch(const string& type, const string& name); // stub for now
+//    void run();
+//};
+//
+//#endif
