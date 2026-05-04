bab04af7ea31f975144cedaa6f7b9b86727c059d
samanfatima15 <samanfatima768@gmail.com>
2026-05-04 19:11:31 +0500

    Update CommandManager.h

--- CommandManager.h
+++ CommandManager.h
@@ -1,30 +1,33 @@
-//#pragma once
-//#ifndef COMMANDMANAGER_H
-//#define COMMANDMANAGER_H
-//
-//#include "Folder.h"
-//#include <string>
-//using namespace std;
-//
-//class CommandManager {
-//private:
-//    Folder* root;
-//    Folder* current;
-//
-//    void searchRecursive(Folder* folder, const string& targetName, const string& path);
-//
-//public:
-//    CommandManager();
-//    ~CommandManager();
-//
-//    void ls();
-//    void mkdir(const string& name);
-//    void cd(const string& name);
-//    void rm(const string& name);
-//    void rename(const string& oldName, const string& newName);
-//    void search(const string& name);
-//    void touch(const string& type, const string& name); // stub for now
-//    void run();
-//};
-//
-//#endif
+#pragma once
+#ifndef COMMANDMANAGER_H
+#define COMMANDMANAGER_H
+
+#include "Folder.h"
+#include "AudioFile.h"
+#include "ZipFile.h"
+#include "TxtFile.h"       
+#include "PrivateFile.h" 
+#include <string>
+using namespace std;
+
+class CommandManager {
+private:
+    Folder* root;
+    Folder* current;
+    void searchRecursive(Folder* folder, const string& targetName, const string& path);
+
+public:
+    CommandManager();
+    ~CommandManager();
+    void ls();
+    void mkdir(const string& name);
+    void cd(const string& name);
+    void rm(const string& name);
+    void rename(const string& oldName, const string& newName);
+    void search(const string& name);
+    void touch(const string& type, const string& name);
+    void unzip(const string& zipName);
+    void run();
+};
+
+#endif
