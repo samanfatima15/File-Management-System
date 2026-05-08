7633a60c17f00b3bbf3204914a55c17521b75abd
samanfatima15 <samanfatima768@gmail.com>
2026-05-08 21:53:39 +0500

    added override  File.h

--- File.h
+++ File.h
@@ -5,25 +5,15 @@
 #include "Node.h"
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-class File : public Node
-{
+class File : public Node {
 public:
-    // Constructor
     File(const string& name, Node* parent);
-
-    // Destructor
     ~File();
-
-    void open() override; 
-
-    // Display file info
-    void display() const;
-
-    // Type check
-    bool isFolder() const;
+    void open() override;
+    void display() const override;    
+    bool isFolder() const override;   
 };
 
 #endif
