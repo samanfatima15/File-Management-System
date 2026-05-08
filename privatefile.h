87df6984045af9a79ea08f9015579fe91f5641b7
samanfatima15 <samanfatima768@gmail.com>
2026-05-08 21:49:33 +0500

    Update(inherit from File) privatefile.h

--- privatefile.h
+++ privatefile.h
@@ -1,17 +1,25 @@
+#pragma once
 #ifndef PRIVATEFILE_H
 #define PRIVATEFILE_H
 
-#include "TxtFile.h"
-
-class PrivateFile : public TxtFile {
+#include "File.h"
+#include <string>
+using namespace std;
+class PrivateFile : public File {
 private:
     string passkey;
+    string* lines;
+    int lineCount;
+    int capacity;
+
+    void expand();
+    bool verifyPasskey();
 
 public:
     PrivateFile(const string& name, Node* parent, const string& passkey);
-
-    bool verifyPasskey();
+    ~PrivateFile();
     void open() override;
+    void display() const override;
 };
 
 #endif
