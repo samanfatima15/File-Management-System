d6fec836759305a99cd548727c1fa9d4cc2c081a
samanfatima15 <samanfatima768@gmail.com>
2026-05-08 21:51:59 +0500

    Update privatefile.cpp

--- privatefile.cpp
+++ privatefile.cpp
@@ -1,31 +1,76 @@
-#include "PrivateFile.h"
+#include "PrivateFile.h"   
 #include <iostream>
-#include "node.h"
-#include "Textfile.h"
 using namespace std;
 
 PrivateFile::PrivateFile(const string& name, Node* parent, const string& passkey)
-    : TxtFile(name + ".priv", parent) {
-
+    : File(name + ".priv", parent)  
     this->passkey = passkey;
+    this->lineCount = 0;
+    this->capacity = 5;
+    this->lines = new string[capacity];
+}
+
+PrivateFile::~PrivateFile() {
+    delete[] lines;
+}
+
+void PrivateFile::expand() {
+    capacity *= 2;
+    string* newLines = new string[capacity];
+    for (int i = 0; i < lineCount; i++)
+        newLines[i] = lines[i];
+    delete[] lines;
+    lines = newLines;
 }
 
 bool PrivateFile::verifyPasskey() {
     string input;
-
     cout << "Enter passkey: ";
-    cin >> input;
-
+    cin >> ws;
+    getline(cin, input);
     return input == passkey;
 }
 
 void PrivateFile::open() {
     if (!verifyPasskey()) {
-        cout << "Wrong passkey.\n";
+        cout << "Wrong passkey. Access denied." << endl;
         return;
     }
+    cout << "Access granted. Editing " << getName() << endl;
 
-    cout << "Access granted.\n";
+    int choice;
+    do {
+        cout << "   Choice:   1. Add Line    2. Edit Line      3. Show Content   0. Close   ";
+        cin >> choice;
+        cin.ignore();
 
-    TxtFile::open();
+        if (choice == 1) {
+            string line;
+            cout << "Enter line: ";
+            getline(cin, line);
+            if (lineCount == capacity) expand();
+            lines[lineCount++] = line;
+        }
+        else if (choice == 2) {
+            int idx;
+            cout << "Line number: ";
+            cin >> idx;
+            cin.ignore();
+            if (idx >= 1 && idx <= lineCount) {
+                cout << "New text: ";
+                getline(cin, lines[idx - 1]);
+            } else {
+                cout << "Invalid line number." << endl;
+            }
+        }
+        else if (choice == 3) {
+            cout << getName() << endl;
+            for (int i = 0; i < lineCount; i++)
+                cout << i + 1 << ". " << lines[i] << endl;
+        }
+    } while (choice != 0);
+}
+
+void PrivateFile::display() const {
+    cout << "[Private] " << name << endl;
 }
