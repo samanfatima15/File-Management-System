f92b4899c800ae101789c3a2904d415859fbe569
Ayesha77314 <ayesharafique1049@gmail.com>
2026-05-12 11:09:48 +0500

    Update config.h

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 
 #include <string>
 
-//the virtual files are created on the disk.
+//the virtual files are here creatd on the disk in the same path of the project we are given
 const std::string VFS_ROOT = "./MyFileSystem/";
 
 #endif
