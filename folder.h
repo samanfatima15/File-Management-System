7ec1018ad06bd9540017d3804116ef9687d0c05b
samanfatima15 <samanfatima768@gmail.com>
2026-05-12 05:37:00 +0500

    Update folder.h

--- folder.h
+++ folder.h
@@ -1,34 +1,32 @@
+#pragma once
 #ifndef FOLDER_H
 #define FOLDER_H
 
 #include "Node.h"
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-class Folder : public Node
-{
+class Folder : public Node {
 private:
-    struct ChildNode
-    {
+    struct ChildNode {
         Node* data;
         ChildNode* next;
     };
-
-    ChildNode* head; 
+    ChildNode* head;
 
 public:
     Folder(const string& name, Node* parent);
     ~Folder();
+
     void addNode(Node* node);
     Node* find(const string& name);
     void removeNode(const string& name);
     void list() const;
-    void open();
-    void display() const;
-    bool isFolder() const;
-    bool searchIn(const string& targetName, const string& path);
+    void open() override;
+    void display() const override;
+    bool isFolder() const override;
+    bool searchNode(const string& target, string path);
 };
 
 #endif
